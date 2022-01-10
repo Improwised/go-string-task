7e7018847ae0ff1046663f73718601459504841d
Munir Khakhi <munir@pandio.com>
2022-01-10 13:07:00 +0530

    Feat: Add storyStats method
    * Correct string in main method

--- main.go
+++ main.go
@@ -8,8 +8,10 @@ import (
 )
 
 func main() {
-	fmt.Println(testValidity("123-qwert-678-567"))
-	fmt.Println(averageNumber("123-qwert-678-567"))
+	fmt.Println(testValidity("123-qwert-678-tgfhg"))
+	fmt.Println(averageNumber("123-qwert-678-tgfhg"))
+	fmt.Println(wholeStory("123-qwert-678-tgfhg"))
+	fmt.Println(storyStats("123-qwert-678-tgfhg"))
 }
 
 // testValidity returns true if given string is matching regex
@@ -55,4 +57,48 @@ func wholeStory(str string) string {
 		return strings.Join(story, " ")
 	}
 	return ""
+}
+
+// storyStats will return shortest word, longest word, average length of all words and words which have average length
+func storyStats(str string) ([]string, []string, int, []string) {
+	var avgLenWords = []string{}
+	var shortestWords []string
+	var shortestWordsLen = len(str)
+	var longestWordsLen = 0
+	var longesttWords []string
+	var avgWordLen = 0
+	var totalWordLen = 0
+	var intCounter = 0
+
+	if testValidity(str) {
+		values := strings.Split(str, "-")
+		for index, val := range values {
+			if index%2 != 0 {
+				// Check is it shortest
+				if len(val) < shortestWordsLen {
+					shortestWordsLen = len(val)
+					shortestWords = []string{val}
+				} else if len(val) == shortestWordsLen {
+					shortestWords = append(shortestWords, val)
+				}
+				// Check is it longest
+				if len(val) > longestWordsLen {
+					longestWordsLen = len(val)
+					longesttWords = []string{val}
+				} else if len(val) == longestWordsLen {
+					longesttWords = append(longesttWords, val)
+				}
+				intCounter++
+				totalWordLen = totalWordLen + len(val)
+			}
+		}
+		avgWordLen = totalWordLen / intCounter
+		for index, val := range values {
+			if index%2 != 0 && len(val) == avgWordLen {
+				avgLenWords = append(avgLenWords, val)
+			}
+		}
+		return shortestWords, longesttWords, avgWordLen, avgLenWords
+	}
+	return []string{}, []string{}, 0, []string{}
 }
