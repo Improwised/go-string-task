734d213bc9e007c849ee17adfded374262561dc0
Munir Khakhi <munir@pandio.com>
2022-01-10 13:13:16 +0530

    test: Add Test_storyStats

--- main_test.go
+++ main_test.go
@@ -39,3 +39,42 @@ func Test_wholeStory(t *testing.T) {
 	assert.Equal(t, "pzd rcgo ucmfeq zhpqwb", wholeStory("3089-pzd-027403-rcgo-469704-ucmfeq-46-zhpqwb"))
 	assert.Equal(t, "", wholeStory("7428"))
 }
+
+func Test_storyStats(t *testing.T) {
+	shortestWords, longestWords, avgLen, avgLenWords := storyStats("50-hhd-50-fgfdh-50-fddhf-150-fddf")
+	assert.Equal(t, []string{"hhd"}, shortestWords)
+	assert.Equal(t, []string{"fgfdh", "fddhf"}, longestWords)
+	assert.Equal(t, 4, avgLen)
+	assert.Equal(t, []string{"fddf"}, avgLenWords)
+
+	shortestWords, longestWords, avgLen, avgLenWords = storyStats("81-mva-395-alwt-608302-nv")
+	assert.Equal(t, []string{"nv"}, shortestWords)
+	assert.Equal(t, []string{"alwt"}, longestWords)
+	assert.Equal(t, 3, avgLen)
+	assert.Equal(t, []string{"mva"}, avgLenWords)
+
+	shortestWords, longestWords, avgLen, avgLenWords = storyStats("7844-nnnfx-2145-xhk")
+	assert.Equal(t, []string{"xhk"}, shortestWords)
+	assert.Equal(t, []string{"nnnfx"}, longestWords)
+	assert.Equal(t, 4, avgLen)
+	assert.Equal(t, []string{}, avgLenWords)
+
+	shortestWords, longestWords, avgLen, avgLenWords = storyStats("02-fvdxe-536-bpxy-346-lfo-556537-cxo-59666-fygx-24615-zsvfli")
+	assert.Equal(t, []string{"lfo", "cxo"}, shortestWords)
+	assert.Equal(t, []string{"zsvfli"}, longestWords)
+	assert.Equal(t, 4, avgLen)
+	assert.Equal(t, []string{"bpxy", "fygx"}, avgLenWords)
+
+	shortestWords, longestWords, avgLen, avgLenWords = storyStats("3089-pzd-027403-rcgo-469704-ucmfeq-46-zhpqwb")
+	assert.Equal(t, []string{"pzd"}, shortestWords)
+	assert.Equal(t, []string{"ucmfeq", "zhpqwb"}, longestWords)
+	assert.Equal(t, 4, avgLen)
+	assert.Equal(t, []string{"rcgo"}, avgLenWords)
+
+	shortestWords, longestWords, avgLen, avgLenWords = storyStats("7428")
+	assert.Equal(t, []string{}, shortestWords)
+	assert.Equal(t, []string{}, longestWords)
+	assert.Equal(t, 0, avgLen)
+	assert.Equal(t, []string{}, avgLenWords)
+
+}
